e6d9a0f2a9698ff6f00db555183fc55bd769e247
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-05 17:02:45 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,17 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class !=
+  Person 
+    raise PartnerError
+  else 
     person.partner = self
   end
-
 end
+
+  class PartnerError < StandardError
+  end 
+end 
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
